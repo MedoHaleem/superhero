c7860e067564dd8039fde38369643f67807ecc04
Medo Haleem <medo.a.haleem@gmail.com>
2025-01-28 19:34:14 +0200

    Add basic endpoints

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,37 @@ import cors from "cors";
 const port = 5000;
 const expressApp: Express = express();
 
+interface Superhero {
+    name: string;
+    superpower: string;
+    humilityScore: number;
+}
+
+// init Memory Storage
+let superheroes: Superhero[] = [];
+
 expressApp.use(cors({
     origin: "*"
 }));
 
 expressApp.use(express.json());
 
-expressApp.get("/", (req, res) => {
-    res.status(200).send("Hello World");
+
+expressApp.post("/superheroes", (req, res) => {
+    const { name, superpower, humilityScore } = req.body;
+    superheroes.push({
+        name,
+        superpower,
+        humilityScore
+    });
+    res.status(201).json({ 
+        message: "Superhero added successfully!", 
+        superhero: { name, superpower, humilityScore }
+    });
+});
+
+expressApp.get("/superheroes", (req, res) => {
+    res.status(200).json(superheroes);
 });
 
 const server = createServer(expressApp);
