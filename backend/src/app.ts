d063bd27def313b6864061e3858e6a7a0a3c22e0
Medo Haleem <medo.a.haleem@gmail.com>
2025-01-29 13:46:59 +0200

    add Helmet for secuirty

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,13 @@ import routes from "./routes/index";
 import middleware from "./middleware/cors";
 import { Superhero } from "./model/superhero";
 import express from "express";
+import helmet from "helmet";
 
 const app = express();
 let superheroes: Superhero[] = [];
 app.use(middleware("http://localhost:5173"));
 app.use(express.json());
+app.use(helmet());
 app.use(routes);
 
 export {superheroes, app}
